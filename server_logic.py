64836e71f96c82cfe5d47b090d3878eaf392c9e7
Abdelmoniem <abdelmoniemmmh@gmail.com>
2021-10-26 15:18:58 -0400

    Build Basic Logic

--- server_logic.py
+++ server_logic.py
@@ -1,5 +1,7 @@
 import random
+import math
 from typing import List, Dict
+
 
 """
 This file can be a nice home for your move logic, and to write helper functions.
@@ -50,10 +52,10 @@ def choose_move(data: dict) -> str:
     my_body = data["you"]["body"]  # A list of x/y coordinate dictionaries like [ {"x": 0, "y": 0}, {"x": 1, "y": 0}, {"x": 2, "y": 0} ]
 
     # TODO: uncomment the lines below so you can see what this data looks like in your output!
-    # print(f"~~~ Turn: {data['turn']}  Game Mode: {data['game']['ruleset']['name']} ~~~")
-    # print(f"All board data this turn: {data}")
-    # print(f"My Battlesnakes head this turn is: {my_head}")
-    # print(f"My Battlesnakes body this turn is: {my_body}")
+    print(f"~~~ Turn: {data['turn']}  Game Mode: {data['game']['ruleset']['name']} ~~~")
+    print(f"All board data this turn: {data}")
+    print(f"My Battlesnakes head this turn is: {my_head}")
+    print(f"My Battlesnakes body this turn is: {my_body}")
 
     possible_moves = ["up", "down", "left", "right"]
 
@@ -61,17 +63,84 @@ def choose_move(data: dict) -> str:
     possible_moves = avoid_my_neck(my_head, my_body, possible_moves)
 
     # TODO: Using information from 'data', find the edges of the board and don't let your Battlesnake move beyond them
-    # board_height = ?
-    # board_width = ?
+    board_height = data['board']['height']
+    board_width = data['board']['height']
+
+    if (my_head['x'] + 1 >= board_width): 
+        possible_moves.remove('right')
+    if (my_head['x'] - 1 < 0):
+        possible_moves.remove('left')
+    if (my_head['y'] + 1 >= board_height):
+        possible_moves.remove('up')
+    if (my_head['y'] - 1 < 0):
+        possible_moves.remove('down')
 
     # TODO Using information from 'data', don't let your Battlesnake pick a move that would hit its own body
 
+    for block in my_body:
+        if (my_head['x'] + 1 == block['x'] and my_head['y'] == block['y']):
+            possible_moves.remove('right')
+        if (my_head['x'] - 1 == block['x'] and my_head['y'] == block['y']):
+            possible_moves.remove('left')
+        if (my_head['x'] == block['x'] and my_head['y'] + 1 == block['y']):
+            possible_moves.remove('up')
+        if (my_head['x'] == block['x'] and my_head['y'] - 1 == block['y']):
+            possible_moves.remove('down')
+
     # TODO: Using information from 'data', don't let your Battlesnake pick a move that would collide with another Battlesnake
+
+    for snake in data['snakes']:
+        for block in snake:
+            if (my_head['x'] + 1 == block['x'] and my_head['y'] == block['y']):
+                possible_moves.remove('right')
+            if (my_head['x'] - 1 == block['x'] and my_head['y'] == block['y']):
+                possible_moves.remove('left')
+            if (my_head['x'] == block['x'] and my_head['y'] + 1 == block['y']):
+                possible_moves.remove('up')
+            if (my_head['x'] == block['x'] and my_head['y'] - 1 == block['y']):
+                possible_moves.remove('down')
+
 
     # TODO: Using information from 'data', make your Battlesnake move towards a piece of food on the board
 
+    closest_food = data['food']['0']
+    food_distance = lambda head, food: math.sqrt((head['x'] - food['x'])**2 + (head['y'] - food['y'])**2)
+    min_distance = food_distance(my_head, closest_food)
+
+    for food in data['board']['food']:
+        temp_distance = food_distance(my_head, food)
+        if temp_distance < min_distance:
+            closest_food = food
+            min_distance = temp_distance
+
+    #choose move that will move you towards the closest food item
+    ideal_moves = possible_moves.copy()
+
+    for move in ideal_moves:
+        if move == 'up':
+            temp_head = my_head.copy()
+            temp_head["y"] += 1
+            if (food_distance(temp_head, closest_food) < min_distance):
+                ideal_moves.remove('up')
+        elif move == 'down':
+            temp_head = my_head.copy()
+            temp_head["y"] -= 1
+            if (food_distance(temp_head, closest_food) < min_distance):
+                ideal_moves.remove('down')
+        elif move == 'right':
+            temp_head = my_head.copy()
+            temp_head["x"] += 1
+            if (food_distance(temp_head, closest_food) < min_distance):
+                ideal_moves.remove('right')
+        else:
+            temp_head = my_head.copy()
+            temp_head["x"] -= 1
+            if (food_distance(temp_head, closest_food) < min_distance):
+                ideal_moves.remove('left')
+
+
     # Choose a random direction from the remaining possible_moves to move in, and then return that move
-    move = random.choice(possible_moves)
+    move = random.choice(ideal_moves) if (ideal_moves) else  random.choice(possible_moves)
     # TODO: Explore new strategies for picking a move that are better than random
 
     print(f"{data['game']['id']} MOVE {data['turn']}: {move} picked from all valid options in {possible_moves}")
